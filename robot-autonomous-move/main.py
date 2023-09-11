f871c79110a2ab974a4a036adb18a83d00438a5d
Szymon Ryś <rysszymon00@gmail.com>
2023-09-11 15:15:35 +0200

    *update temp main version

--- robot-autonomous-move/main.py
+++ robot-autonomous-move/main.py
@@ -19,8 +19,21 @@ from hokuyo_lib.tools import serial_port
 UART_PORT = '/dev/ttyACM0'
 UART_SPEED = 19200
 
-def choose_direction():
-    pass
+defaultMiabotPort = "/dev/ttyS0"
+defaultBaudrate = 115200
+
+rotate_right = b"[n60]\n"
+step_len = b"[d^200000]\n"
+step_fwd = b"[^]\n"
+
+ser = serial.Serial(
+    port=defaultMiabotPort,
+    baudrate=defaultBaudrate,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
 
 if __name__ == '__main__':
     laser_serial = serial.Serial(port=UART_PORT, baudrate=UART_SPEED, timeout=0.5)
@@ -28,10 +41,11 @@ if __name__ == '__main__':
 
     laser = hokuyo.Hokuyo(port)
 
-    front_angle: int = 10
+    front_angle: int = 30
     turn_while_obstacle_is_closed_than: int = 200       # 200 is equal to 20 cm
 
     print(laser.set_high_sensitive())
+    ser.write(str.encode(step_len))
 
     while True:
         print(laser.laser_on())
@@ -49,8 +63,10 @@ if __name__ == '__main__':
 
         if front_obtacle_distance < turn_while_obstacle_is_closed_than:
             print("I will change my direction, measured dist:", front_obtacle_distance)
+            ser.write(str.encode(rotate_right))
         else:
             print("I will move forward, measured dist:", front_obtacle_distance)
+            ser.write(str.encode(step_fwd))
 
         # print(dumps(fixed_data, indent=4))
         print(laser.laser_off())
