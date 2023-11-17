f6e19e44c11e648bdc5d3e0be02bdc83ad5f3663
miloszdobosz <mdobosz@student.agh.edu.pl>
2023-11-17 13:53:26 +0100

    MiabotNode command translation maybe works

--- nodes/miabot/miabot_node.py
+++ nodes/miabot/miabot_node.py
@@ -1,31 +1,67 @@
 import rclpy
+import serial
+import struct
 from rclpy.node import Node
 from geometry_msgs.msg import Twist, Point, Quaternion
 from std_msgs.msg import String
 
+# TODO
+LINEAR_FACTOR = 1
+ANGULAR_FACTOR = 1
+
+SERIAL_PORT = "/dev/ttyACM0"
+BAUD_RATE = 115200
+
+FORWARD = "[o{}]"
+BACKWARD = "[p{}]"
+LEFT = "[m{}]"
+RIGHT = "[n{}]"
+
 class MiaBotNode(Node):
     def __init__(self):
-        super().__init__('miabot_node')
-        self.cmd_val_subscriber = self.create_subscription(String, '/cmd_val', self.cmd_val_callback, 10)
-        self.odom_publisher = self.create_publisher(Twist, '/odom', 10)
+        super().__init__("miabot_node")
+        self.cmd_val_subscriber = self.create_subscription(Twist, "cmd_val", self.cmd_val_callback, 10)
+        self.odom_publisher = self.create_publisher(Twist, "odom", 10)
+        self.output = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
 
-    def cmd_val_callback(self, msg):
-        # Translate /cmd_val messages to robot commands
-        # Your implementation here
+    def cmd_val_callback(self, msg: Twist):
+        self.get_logger().debug(f"""
+                                Received movement command:
+                                \n\tLinear:
+                                \t{msg.linear.x},
+                                \t{msg.linear.y},
+                                \t{msg.linear.x},
+                                \n\tAngular:
+                                \t{msg.angular.x},
+                                \t{msg.angular.y},
+                                \t{msg.angular.x},
+                                \n""")
 
-    def send_serial_messages(self):
-        # Send serial messages to /dev/ttyACM0 at baud 115200
-        # Your implementation here
+        linear = msg.linear.x * LINEAR_FACTOR
+        angular = msg.angular.z * ANGULAR_FACTOR
+
+        self.write(
+            FORWARD.format(linear) if linear > 0 else BACKWARD.format(-linear) +
+            LEFT.format(linear) if linear > 0 else RIGHT.format(-linear)
+        )
+
+    def write(self, message):
+        self.output.write(message.encode("utf-8"))
 
     def publish_odom(self):
         # Simulate publishing /odom messages similar to turtlebot
         # Your implementation here
+        self.odom_publisher.publish(Twist())
+
+    def destroy_node(self):
+        self.output.close()
+        super().destroy_node()
 
 def main(args=None):
     rclpy.init(args=args)
     miabot_node = MiaBotNode()
 
-    # Implement your node's logic here (e.g., running a loop to listen to messages and perform actions)
+    # Implement your node"s logic here (e.g., running a loop to listen to messages and perform actions)
 
     rclpy.spin(miabot_node)
 
@@ -33,6 +69,6 @@ def main(args=None):
     miabot_node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
