014bacfa602e1e927dc72ed2278190b8515c4c97
mrrys00 <rysszymon00@gmail.com>
2023-11-28 21:13:41 +0100

    +changes in config

--- nodes/miabot_node/miabot_node/miabot_node.py
+++ nodes/miabot_node/miabot_node/miabot_node.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, TransformStamped
+from tf2_msgs.msg import TFMessage
 from nav_msgs.msg import Odometry
 from time import sleep, time_ns
 
@@ -11,7 +12,7 @@ import serial
 # TODO
 LINEAR_FACTOR = 500         # 1 real meter = 1.0 /cmd_vel = 25317 robot units
 DELTA_RADIUS = 0.03         # distance between singe wheel and robot center
-ODOM_FREQUENCY = 2**1       # odometry messages per second -> optimal 2**4 (?)
+ODOM_FREQUENCY = 2**4       # odometry messages per second -> optimal 2**4 (?)
 
 MAX_SAFE_VELOCITY = 40
 # ANGULAR_FACTOR = 1
@@ -27,7 +28,7 @@ class MiaBotNode(Node):
     def __init__(self):
         super().__init__('miabot_node')
 
-        self.init_time = time_ns()
+        # self.init_time = time_ns()
 
         # Subscribe /cmd_vel
         self.cmd_vel_subscription = self.create_subscription(
@@ -46,13 +47,38 @@ class MiaBotNode(Node):
             Odometry,
             '/odom',
             10)
-        self.odom_timer = self.create_timer(1.0 / ODOM_FREQUENCY, self.publish_odom)
+        self.tf_publisher = self.create_publisher(
+            TFMessage,
+            '/tf',
+            10)
+        self.publish_timer = self.create_timer(1.0 / ODOM_FREQUENCY, self.publish_odom_tf)
+
+        def _init_transform() -> TransformStamped:
+            transform = TransformStamped()
+            transform.header.frame_id = 'odom'
+            transform.header.stamp = self.get_clock().now().to_msg()
+
+            transform.child_frame_id = 'base_footprint'
+            
+            # actual position
+            transform.transform.translation.x = 0.0     # actual x
+            transform.transform.translation.y = 0.0     # actual y
+            transform.transform.translation.z = 0.0     # always 0
+
+            # actual rotation
+            transform.transform.rotation.x = 0.0        # always 0
+            transform.transform.rotation.y = 0.0        # always 0
+            transform.transform.rotation.z = 0.0        # actual radian rotation
+            transform.transform.rotation.w = 1.0        # always 1.0
+
+            return transform
 
         def _init_odometry() -> Odometry:
             odom_msg = Odometry()
             odom_msg.header.frame_id = "odom"
-            odom_msg.header.stamp.sec = 0
-            odom_msg.header.stamp.nanosec = 0
+            odom_msg.header.stamp = self.get_clock().now().to_msg()
+
+            odom_msg.child_frame_id = "base_footprint"
 
             odom_msg.pose.pose.position.x = 0.0     # actual x
             odom_msg.pose.pose.position.y = 0.0     # actual y
@@ -63,17 +89,18 @@ class MiaBotNode(Node):
             odom_msg.pose.pose.orientation.z = 0.0  # actual radian rotation
             odom_msg.pose.pose.orientation.w = 1.0  # always 1.0
 
-            odom_msg.twist.twist.linear.x = 0.0     # linear
+            odom_msg.twist.twist.linear.x = 0.0     # linear velocity
             odom_msg.twist.twist.linear.y = 0.0     # always 0
             odom_msg.twist.twist.linear.z = 0.0     # always 0
 
             odom_msg.twist.twist.angular.x = 0.0    # always 0
             odom_msg.twist.twist.angular.y = 0.0    # always 0
-            odom_msg.twist.twist.angular.z = 0.0    # angular
+            odom_msg.twist.twist.angular.z = 0.0    # angular valocity
 
             return odom_msg
 
         self.odom_msg = _init_odometry()
+        self.transform_msg = _init_transform()
 
 
     def cmd_vel_callback(self, msg: Twist):
@@ -101,30 +128,35 @@ class MiaBotNode(Node):
 
     def update_robot_pose_info(self, duration: float):
         """
-        Calculates 
+        Calculates data to /tf and /odom topics 
         """
-        actual_time = time_ns() - self.init_time
-        self.odom_msg.header.stamp.sec = int(actual_time // 10**9)
-        self.odom_msg.header.stamp.nanosec = actual_time % 10**9
+        self.odom_msg.header.stamp = self.get_clock().now().to_msg()
 
         x_linear = self.odom_msg.twist.twist.linear.x
         z_angular = self.odom_msg.twist.twist.angular.z
         z_oriantation = self.odom_msg.pose.pose.orientation.z
 
-        self.odom_msg.pose.pose.position.x += cos(z_oriantation) * (x_linear*duration)      # actual x
-        self.odom_msg.pose.pose.position.y += sin(z_oriantation) * (x_linear*duration)      # actual y
+        self.odom_msg.pose.pose.position.x += cos(z_oriantation) * (x_linear*duration)      # actual x 2d
+        self.odom_msg.pose.pose.position.y += sin(z_oriantation) * (x_linear*duration)      # actual y 2d
 
-        self.odom_msg.pose.pose.orientation.z += (z_angular*duration) % (2*pi)
+        self.odom_msg.pose.pose.orientation.z += (z_angular*duration) % (2*pi)              # actual z rotation
 
-        self.odom_msg = self.odom_msg
+        self.transform_msg.transform.translation.x = self.odom_msg.pose.pose.position.x     # actual x 2d
+        self.transform_msg.transform.translation.y = self.odom_msg.pose.pose.position.y     # actual y 2d
 
-    def publish_odom(self):
+        self.transform_msg.transform.rotation.z = self.odom_msg.pose.pose.orientation.z     # actual z rotation
+
+    def publish_odom_tf(self):
         """
         Publishes messages to the /odom topic
         """
         self.update_robot_pose_info(1.0 / ODOM_FREQUENCY)
 
-        self.odom_publisher.publish(self.odom_msg)
+        tf_message = TFMessage()
+        tf_message.transforms.append(self.transform_msg)
+
+        self.odom_publisher.publish(self.odom_msg)      # frame_id: odom; child_frame_id: base_footprint
+        self.tf_publisher.publish(tf_message)   # frame_id: odom; child_frame_id: base_footprint
         self.get_logger().info(f'Odom\nlin x: {self.odom_msg.twist.twist.linear.x}\n ang z: {self.odom_msg.twist.twist.angular.z}')
 
     def process_cmd_vel_to_wheels(
@@ -150,8 +182,7 @@ class MiaBotNode(Node):
 
     def write(self, msg: str):
         """
-        Writes data to serial port
-        Typically it is left and right speed but it is possible to send custom message
+        Writes data to serial port.
         """
         try:
             self.serial_port.write(msg.encode('utf-8'))
